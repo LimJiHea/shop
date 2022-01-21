688e49f3b7eb3928dc55973c9be82470a8940391
LJH <akdi22@hanmail.net>
2022-01-21 13:59:04 +0900

    p202 다대일 일대다 양방향 매핑

--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")         //정렬시 사용하는 order 키워드가 있기 때문에 orders로 지정
@@ -26,6 +28,12 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;    //주문상태
+
+    @OneToMany(mappedBy = "order")             //주문 상품 엔티티와 일대다 매핑을 한다.
+                                                //외래키(order_id)가 order_item 테이블에 있으므로 연관관계의 주인은 OrderItem 엔티티이다. 
+                                                //Order 엔티티가 준인이 아니므로 (mappedBy)속성으로 연관관계의 주인을 설정
+                                                //속성의 값으로 "order"를 적어준 이유는 OrderItem에 있는 Order에 의해 관리된다는 의미
+    private List<OrderItem> orderItems = new ArrayList<>();         //하나의 주문이 여러개의 주문 상품을 갖으므로 List 자료형을 사용해서 매핑
 
     private LocalDateTime regTime;
 
