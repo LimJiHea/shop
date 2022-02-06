8850d83a2b33611954c0400965201bbe30fa53f0
임지혜 <akdi22@hanmail.net>
2022-02-06 20:30:00 +0900

    p249 ItemService

--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -24,18 +24,18 @@ public class ItemService {
     public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception{
 
         //상품 등록
-        Item item = itemFormDto.createItem();
-        itemRepository.save(item);
+        Item item = itemFormDto.createItem();       //상품 등록 폼으로부터 입력 받은 데이터를 이용하여 item객체 생성
+        itemRepository.save(item);          //상품 데이터 저장
 
         //이미지 등록
         for(int i=0; i<itemImgFileList.size(); i++){
             ItemImg itemImg = new ItemImg();
             itemImg.setItem(item);
-            if(i==0)
+            if(i==0)                            //첫번째 이미지일 경우 대표 상품 이미지 여부 값을 "Y"로 세팅, 나머지는 "N"
                 itemImg.setRepimgYn("Y");
             else
                 itemImg.setRepimgYn("N");
-            itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
+            itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));            //상품의 이미지 정보를 저장한다.
         }
 
         return item.getId();
