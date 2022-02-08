457cd93307f57fa7e635ee6f89a1a450a00e7d09
LJH <akdi22@hanmail.net>
2022-02-08 17:35:16 +0900

    p255 상품 등록 테스트

--- src/test/java/com/shop/service/ItemServiceTest.java
+++ src/test/java/com/shop/service/ItemServiceTest.java
@@ -36,12 +36,12 @@ class ItemServiceTest {
     @Autowired
     ItemImgRepository itemImgRepository;
 
-    List<MultipartFile> createMultipartFiles() throws Exception{
+    List<MultipartFile> createMultipartFiles() throws Exception{        //MockMultipartFile 클래스를 이용해 가짜 MultipartFile 리스트를 만들어 반환해주는 메소드
 
         List<MultipartFile> multipartFileList = new ArrayList<>();
 
         for(int i=0; i<5; i++){
-            String path = "C:/shop/item/";
+            String path = "C:/LJH/shop/item/";
             String imageName = "image" + i + ".jpg";
             MockMultipartFile multipartFile = new MockMultipartFile(path, imageName, "image/jpg", new byte[]{1,2,3,4});
             multipartFileList.add(multipartFile);
@@ -55,7 +55,7 @@ class ItemServiceTest {
     @WithMockUser(username = "admin", roles = "ADMIN")
     void saveItem() throws Exception{
 
-        ItemFormDto itemFormDto = new ItemFormDto();
+        ItemFormDto itemFormDto = new ItemFormDto();        //상품 등록 화면에서 입력 받는 상품 데이터를 세팅해준다.
         itemFormDto.setItemNm("테스트 상품");
         itemFormDto.setItemSellStatus(ItemSellStatus.SELL);
         itemFormDto.setItemDetail("테스트 상품 입니다.");
@@ -63,18 +63,19 @@ class ItemServiceTest {
         itemFormDto.setStockNumber(100);
 
         List<MultipartFile> multipartFileList = createMultipartFiles();
-        Long itemId = itemService.saveItem(itemFormDto, multipartFileList);
+        Long itemId = itemService.saveItem(itemFormDto, multipartFileList);     //상품 데이터와 이미지 정보를 파라미터로 넘겨서 저장 후 저장된 상품의 아이디 값을 반환 값으로 리턴
 
         List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
         Item item = itemRepository.findById(itemId)
                 .orElseThrow(EntityNotFoundException::new);
 
-        assertEquals(itemFormDto.getItemNm(), item.getItemNm());
+        assertEquals(itemFormDto.getItemNm(), item.getItemNm());        //입력한 상품 데이터와 실제로 저장된 상품 데이터가 같은지 확인한다.
         assertEquals(itemFormDto.getItemSellStatus(),item.getItemSellStatus());
         assertEquals(itemFormDto.getItemDetail(), item.getItemDetail());
         assertEquals(itemFormDto.getPrice(), item.getPrice());
         assertEquals(itemFormDto.getStockNumber(), item.getStockNumber());
         assertEquals(multipartFileList.get(0).getOriginalFilename(), itemImgList.get(0).getOriImgName());
+        //이미지는 첫 번째 파일의 원본 이미지 파일 이름만 같은지 확인함
 
     }
 }
