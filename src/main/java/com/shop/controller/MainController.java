c7eb837148401515a97d63a65723a82ac2a51df6
LJH <akdi22@hanmail.net>
2022-03-11 17:36:44 +0900

    p288 img

--- src/main/java/com/shop/controller/MainController.java
+++ src/main/java/com/shop/controller/MainController.java
@@ -6,11 +6,11 @@ import com.shop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.awt.print.Pageable;
 import java.util.Optional;
 
 @Controller
@@ -26,9 +26,10 @@ public class MainController {
 
     @GetMapping(value = "/")
     public String main(ItemSearchDto itemSearchDto, Optional<Integer> page, Model model){
+
         Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0,6);
         Page<MainItemDto> items =
-                itemService.getMainItemPage(ItemSearchDto, pageable);
+                itemService.getMainItemPage(itemSearchDto, pageable);
         model.addAttribute("items",items);
         model.addAttribute("itemSearchDto",itemSearchDto);
         model.addAttribute("maxPage",5);
