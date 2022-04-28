8b3c95966a86fcc2fe77734cd5b9085066e9b412
LJH <akdi22@hanmail.net>
2022-04-28 23:28:47 +0900

    p334 장바구니

--- src/main/java/com/shop/entity/CartItem.java
+++ src/main/java/com/shop/entity/CartItem.java
@@ -25,4 +25,16 @@ public class CartItem extends BaseEntity{
     private Item item;      //장바구니에 담을 상품의 정보를 알아야 하므로 상품엔티티 매핑
 
     private int count;          //같은 상품을 장바구니에 몇 개 담을지 저장
+
+    public static CartItem createCartItem(Cart cart, Item item, int count){
+        CartItem cartItem = new CartItem();
+        cartItem.setCart(cart);
+        cartItem.setItem(item);
+        cartItem.setCount(count);
+        return cartItem;
+    }
+
+    public void addCount(int count){            //기존에 담겨 있는 상품인데, 상품을 추가로 장바구니에 담을 때 기존 수량에 현재 담을 수량을 더 해줄때 사용할 메소드입니다.
+        this.count += count;
+    }
 }
