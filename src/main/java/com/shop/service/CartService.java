46ad26963588851d2325ed83a653e9b76bb45709
LJH <akdi22@hanmail.net>
2022-04-29 23:36:45 +0900

    p336 장바구니

--- src/main/java/com/shop/service/CartService.java
+++ src/main/java/com/shop/service/CartService.java
@@ -27,24 +27,26 @@ public class CartService {
 
     public Long addCart(CartItemDto cartItemDto, String email){
 
-        Item item = itemRepository.findById(cartItemDto.getItemId())
+        Item item = itemRepository.findById(cartItemDto.getItemId())            //장바구니에 담을 상품 엔티티를 조회한다.
                 .orElseThrow(EntityNotFoundException::new);
-        Member member = memberRepository.findByEmail(email);
+        Member member = memberRepository.findByEmail(email);        //현재 로그인한 회원 엔티티를 조회한다.
 
-        Cart cart = cartRepository.findByMemberId(member.getId());
-        if(cart == null){
+        Cart cart = cartRepository.findByMemberId(member.getId());      //현재 로그인한 회원의 장바구니 엔티티를 조회한다
+        if(cart == null){                   //만약 상품을 처음으로 담을 경우 해당 회원의 장바구니 엔티티를 생성한다.
             cart = Cart.createCart(member);
             cartRepository.save(cart);
         }
 
+        //현재 상품이 장바구니에 이미 들어가있는지 조회한다.
         CartItem savedCartItem = cartItemRepository.findByCartIdAndItemId(cart.getId(), item.getId());
 
-        if(savedCartItem != null){
+        if(savedCartItem != null){          //만약 장바구니에 이미 있던 상품일 경우 기존 수량에 현재 장바구니에 담을 수량 만큼을 더해준다.
             savedCartItem.addCount(cartItemDto.getCount());
             return savedCartItem.getId();
         }else{
+            //장바구니 엔티티, 상품 엔티티, 장바구니에 담을 수량을 이용하여 CartItem 엔티티를 생성한다.
             CartItem cartItem = CartItem.createCartItem(cart, item, cartItemDto.getCount());
-            cartItemRepository.save(cartItem);
+            cartItemRepository.save(cartItem);      //장바구니에 들어갈 상품을 저장한다.
             return cartItem.getId();
         }
     }
