4a9c3bc477bd59aab412c8a76c90670cddcc9b64
LJH <akdi22@hanmail.net>
2022-05-06 21:48:12 +0900

    p343 장바구니 조회

--- src/main/java/com/shop/repository/CartItemRepository.java
+++ src/main/java/com/shop/repository/CartItemRepository.java
@@ -1,9 +1,25 @@
 package com.shop.repository;
 
+import com.shop.dto.CartDetailDto;
 import com.shop.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     CartItem findByCartIdAndItemId(Long cartId, Long itemId);       //카트 아이디와 상품 아이디를 이용해서 상품이 장바구니에 들어있는지 조회한다.
+
+    
+    //CartDetailDto의 생성자를 이용하여 DTO를 반환할 때 "new 패키지+클래스명(파라미터순서 = DTO 클래스의 명시한 순서)
+    @Query("select new com.shop.dto.CartDetailDto(ci.id, i.itemNm, i.price, ci.count, im.imgUrl) " +
+            "from CartItem ci, ItemImg im " +
+            "join ci.item i " +
+            "where ci.cart.id = :cartId " +
+            "and im.item.id = ci.item.id " +                        //장바구니에 담겨있는 상품에 대표이미지만 가져오도록 조건문 세팅
+            "and im.repimgYn = 'Y' " +
+            "order by ci.regTime desc"
+    )
+    List<CartDetailDto> findCartDetailDtoList(Long cartId);
 }
--- src/test/java/com/shop/service/CartServiceTest.java
+++ src/test/java/com/shop/service/CartServiceTest.java
@@ -1,6 +1,8 @@
 package com.shop.service;
 
 import com.shop.constant.ItemSellStatus;
+import com.shop.dto.CartItemDto;
+import com.shop.entity.CartItem;
 import com.shop.entity.Item;
 import com.shop.entity.Member;
 import com.shop.repository.CartItemRepository;
@@ -12,6 +14,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
@@ -30,7 +36,7 @@ class CartServiceTest {
     @Autowired
     CartItemRepository cartItemRepository;
 
-    public Item saveItem(){
+    public Item saveItem(){                                     //장바구니에 담을 상품 저장하는 메소드
         Item item = new Item();
         item.setItemNm("테스트 상품");
         item.setPrice(10000);
@@ -40,7 +46,7 @@ class CartServiceTest {
         return itemRepository.save(item);
     }
 
-    public Member saveMember(){
+    public Member saveMember(){                     //회원 정보를 저장하는 메소드
         Member member = new Member();
         member.setEmail("test@test.com");
         return memberRepository.save(member);
@@ -48,5 +54,24 @@ class CartServiceTest {
 
     @Test
     @DisplayName("장바구니 담기 테스트")
-    public
+    public void addCart(){
+        Item item = saveItem();
+
+        Member member = saveMember();
+
+        CartItemDto cartItemDto = new CartItemDto();
+
+        cartItemDto.setCount(5);                    //장바구니에 담을 수량과 상품을 Dto 객체에 세팅
+        cartItemDto.setItemId(item.getId());
+
+        //상품을 장바구니에 담는 로직 호출 결과 생성된 장바구니 상품 아이디를 cartItemId 변수에 저장한다.
+        Long cartItemId = cartService.addCart(cartItemDto, member.getEmail());
+
+
+        CartItem cartItem = cartItemRepository.findById(cartItemId)     //장바구니 상품 아이디를 이용하여 생성된 장바구니 상품 정보를 조회한다.
+                .orElseThrow(EntityNotFoundException::new);
+
+        assertEquals(item.getId(), cartItem.getItem().getId());     //상품 아이디 == 장바구니에 저장된 상품 아이디 (테스트 통과)
+        assertEquals(cartItemDto.getCount(), cartItem.getCount());  //장바구니에 담았던 수량 == 실제로 장바구니에 저장된 수량 (테스트 통과)
+    }
 }
